055f670198340c0999f1abf87328d59b7428be10
Indrajeet <indrajeetseru@gmail.com>
2021-05-30 17:58:28 +0530

    Added : given three number when second highest then return maximum

---
+++ src/com/Generics/Generics_Test_Maximum/FindMaximum.java
@@ -0,0 +1,18 @@
+package com.Generics.Generics_Test_Maximum;
+
+public class FindMaximum {
+    public static void main(String[] args) {
+        System.out.println("*************** Welcome to Generics ***************");
+    }
+    // Give 3 integer number and find maximum number
+    public Integer maxnumber(Integer firstNumber,Integer secondNumber,Integer thardNumber){
+        Integer maxNumber = firstNumber;
+        if(secondNumber.compareTo(maxNumber)>0){
+            maxNumber = secondNumber;
+        }
+        if(thardNumber.compareTo(maxNumber)>0){
+            maxNumber = thardNumber;
+        }
+        return maxNumber;
+    }
+}
