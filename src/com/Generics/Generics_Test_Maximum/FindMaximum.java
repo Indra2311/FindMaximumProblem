b22a39866dfacdd2da2aa2dfd244d5f50bd4e70a
Indrajeet <indrajeetseru@gmail.com>
2021-05-30 18:04:06 +0530

    Three Float number Check

--- src/com/Generics/Generics_Test_Maximum/FindMaximum.java
+++ src/com/Generics/Generics_Test_Maximum/FindMaximum.java
@@ -1,18 +1,29 @@
 package com.Generics.Generics_Test_Maximum;
 
 public class FindMaximum {
-    public static void main(String[] args) {
-        System.out.println("*************** Welcome to Generics ***************");
-    }
-    // Give 3 integer number and find maximum number
-    public Integer maxnumber(Integer firstNumber,Integer secondNumber,Integer thardNumber){
-        Integer maxNumber = firstNumber;
-        if(secondNumber.compareTo(maxNumber)>0){
-            maxNumber = secondNumber;
-        }
-        if(thardNumber.compareTo(maxNumber)>0){
-            maxNumber = thardNumber;
-        }
-        return maxNumber;
-    }
+	 public static void main(String[] args) {
+	        System.out.println("  Welcome to Generics ");
+	    }
+
+	 public Integer maxnumber(Integer firstNumber,Integer secondNumber,Integer thardNumber){
+	        Integer maxNumber = firstNumber;
+	        if(secondNumber.compareTo(maxNumber)>0){
+	            maxNumber = secondNumber;
+	        }
+	        if(thardNumber.compareTo(maxNumber)>0){
+	            maxNumber = thardNumber;
+	        }
+	        return maxNumber;
+	    }
+
+	    public Float maxFloatNumber(Float firstNumber,Float secondNumber,Float thardNumber){
+	        Float maxNumber = firstNumber;
+	        if(secondNumber.compareTo(maxNumber)>0){
+	            maxNumber = secondNumber;
+	        }
+	        if(thardNumber.compareTo(maxNumber)>0){
+	            maxNumber = thardNumber;
+	        }
+	        return maxNumber;
+	    }
 }
