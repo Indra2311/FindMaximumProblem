861f62f672fdca22288e1a1a122e59df2670ddb3
Indrajeet <indrajeetseru@gmail.com>
2021-05-30 18:07:52 +0530

    All test cases & Params

--- src/com/Generics/Generics_Test_Maximum/FindMaximum.java
+++ src/com/Generics/Generics_Test_Maximum/FindMaximum.java
@@ -1,29 +1,34 @@
 package com.Generics.Generics_Test_Maximum;
 
-public class FindMaximum {
-	 public static void main(String[] args) {
-	        System.out.println("  Welcome to Generics ");
-	    }
+public class FindMaximum <T extends Comparable <T>>{
 
-	 public Integer maxnumber(Integer firstNumber,Integer secondNumber,Integer thardNumber){
-	        Integer maxNumber = firstNumber;
-	        if(secondNumber.compareTo(maxNumber)>0){
-	            maxNumber = secondNumber;
-	        }
-	        if(thardNumber.compareTo(maxNumber)>0){
-	            maxNumber = thardNumber;
-	        }
-	        return maxNumber;
-	    }
+    // Variables
+    T firstVariable;
+    T secondVariable;
+    T thardVariable;
 
-	    public Float maxFloatNumber(Float firstNumber,Float secondNumber,Float thardNumber){
-	        Float maxNumber = firstNumber;
-	        if(secondNumber.compareTo(maxNumber)>0){
-	            maxNumber = secondNumber;
-	        }
-	        if(thardNumber.compareTo(maxNumber)>0){
-	            maxNumber = thardNumber;
-	        }
-	        return maxNumber;
-	    }
+    public MaximumProblemUsingGenerics(T firstVariable, T secondVariable, T thardVariable) {
+        this.firstVariable = firstVariable;
+        this.secondVariable = secondVariable;
+        this.thardVariable = thardVariable;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(" Welcome to Generics ");
+    }
+
+    public <T extends Comparable <T>> T  getMaximumNumber(T firstValue,T secondValue, T thardValue){
+        T maximumResult = firstValue;
+        if(secondValue.compareTo(maximumResult)>0){
+            maximumResult = secondValue;
+        }
+        if(thardValue.compareTo(maximumResult)>0){
+            maximumResult = thardValue;
+        }
+        return maximumResult;
+    }
+    public T getMaximumNumber(){
+        return getMaximumNumber(firstVariable,secondVariable,thardVariable);
+
+    }
 }
